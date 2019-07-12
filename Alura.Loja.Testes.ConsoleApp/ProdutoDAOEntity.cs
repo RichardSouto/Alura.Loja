6f66735dd167287f2164135cc63116ddee2e3114
Richard Souto <richard.ssouto@gmail.com>
2019-07-11 22:42:04 -0300

    Classe produto dao entity inicializada

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class ProdutoDAOEntity : IDisposable, IProdutoDAO
+    {
+        public void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
