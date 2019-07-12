2516a46568e222848d48164acedba10db0e196df
Richard Souto <richard.ssouto@gmail.com>
2019-07-11 21:08:53 -0300

    Inicialização da Interface DAO com Entity

---
+++ Alura.Loja.Testes.ConsoleApp/IProdutoDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    interface IProdutoDAO
+    {
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+        void Remover(Produto produto);
+        IList<Produto> Produtos();
+    }
+}
