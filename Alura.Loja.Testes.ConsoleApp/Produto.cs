10ce0761e736f01b79fb8ff165bbeb363a943a9e
Richard Souto <richard.ssouto@gmail.com>
2019-07-14 19:30:23 -0300

    Atulização de arquivos que deverão ser ignorados. Arquivos contidos na pasta package

--- Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,6 +5,12 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
+        public string Unidade { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitario}";
+        }
     }
 }
