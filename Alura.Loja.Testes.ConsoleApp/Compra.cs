5793625e9a44b6ca0cc3b89568ce1efbecf15f0d
Richard Souto <richard.ssouto@gmail.com>
2019-07-16 00:00:28 -0300

    Insercao do campo ProdutoID para corrigir o modelo que sera sincronizado na migracao do banco

--- Alura.Loja.Testes.ConsoleApp/Compra.cs
+++ Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -8,6 +8,7 @@
 
         public int Id { get; internal set; }
         public int Quantidade { get; internal set; }
+        public int ProdutoID { get; internal set; }
         public Produto Produto { get; internal set; }
         public double Preco { get; internal set; }
     }
