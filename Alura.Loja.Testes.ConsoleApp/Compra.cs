569c523c661ad7f4c924274675eebdce2f1a1e52
Richard Souto <richard.ssouto@gmail.com>
2019-07-15 23:54:37 -0300

    Inicializacao da modelagem da classe que define uma compra.

---
+++ Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -0,0 +1,14 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Compra
+    {
+        public Compra()
+        {
+        }
+
+        public int Id { get; internal set; }
+        public int Quantidade { get; internal set; }
+        public Produto Produto { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
