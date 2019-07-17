d3b16f7d99a55b837d84ed59ffdbc7c45e5865ed
Richard Souto <richard.ssouto@gmail.com>
2019-07-16 23:17:10 -0300

    Criacao de classe que representa o Inner Join de N para N entre as classes promocoes e produtos

---
+++ Alura.Loja.Testes.ConsoleApp/PromocaoProduto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class PromocaoProduto
+    {
+        public int ProdutoID { get; set; }
+        public Produto Produto { get; set; }
+        public int PromocaoID { get; set; }
+        public Promocao Promocao { get; set; }
+    }
+}
