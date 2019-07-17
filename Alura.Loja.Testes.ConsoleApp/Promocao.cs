258bd2939cf35debb80b248ca3de0ea9b64f6e2c
Richard Souto <richard.ssouto@gmail.com>
2019-07-16 22:55:55 -0300

    Inicio modelo de uma promocao

---
+++ Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Promocao
+    {
+        public string Descricao { get; internal set; }
+        public object DataInicio { get; internal set; }
+        public DateTime DataTermino { get; internal set; }
+        public IList<Produto> Produtos { get; internal set; }
+    }
+}
