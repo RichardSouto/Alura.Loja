ede347bb4b1e2bb07e05a9775ae52f7cbb1514aa
Richard Souto <richard.ssouto@gmail.com>
2019-07-21 21:48:09 -0300

    Inclusao do Metodo Incluir Produto

--- Alura.Loja.Testes.ConsoleApp/Promocao.cs
+++ Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -10,5 +10,15 @@ namespace Alura.Loja.Testes.ConsoleApp
         public DateTime DataInicio { get; internal set; }
         public DateTime DataTermino { get; internal set; }
         public IList<PromocaoProduto> Produtos { get; internal set; }
+
+        public Promocao()
+        {
+            this.Produtos = new List<PromocaoProduto>();
+        }
+
+        internal void IncluirProduto(Produto produto)
+        {
+            this.Produtos.Add(new PromocaoProduto() { Produto = produto });
+        }
     }
 }
