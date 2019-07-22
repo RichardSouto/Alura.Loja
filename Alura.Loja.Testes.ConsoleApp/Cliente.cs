668b04beb0d59f0bb04087223c41a7f40274c74b
Richard Souto <richard.ssouto@gmail.com>
2019-07-21 22:07:04 -0300

    Inicialização classe cliente

---
+++ Alura.Loja.Testes.ConsoleApp/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Cliente
+    {
+        public int Id { get; set };
+        public string Nome { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
