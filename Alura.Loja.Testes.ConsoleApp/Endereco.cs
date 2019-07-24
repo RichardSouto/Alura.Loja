f869bc6c45fbf25dfb6ac92318ce898a6317c39e
Richard Souto <richard.ssouto@gmail.com>
2019-07-23 23:27:38 -0300

    Inclusao da propriedade ID do Cliente. Shadow Property não funcionou pelo Entity

--- Alura.Loja.Testes.ConsoleApp/Endereco.cs
+++ Alura.Loja.Testes.ConsoleApp/Endereco.cs
@@ -2,11 +2,13 @@
 {
     public class Endereco
     {
+        private int ClienteID { get; set; }
         public int Numero { get; internal set; }
         public string Logradouro { get; internal set; }
         public string Complemento { get; internal set; }
         public string Bairro { get; internal set; }
         public string Cidade { get; internal set; }
         public string Estado { get; internal set; }
+        public Cliente Cliente { get; internal set; }
     }
 }
