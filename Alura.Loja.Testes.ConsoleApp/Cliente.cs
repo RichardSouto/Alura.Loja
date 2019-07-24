4be218f1dbc2b9d480af2014ea08cef2d9387a49
Richard Souto <richard.ssouto@gmail.com>
2019-07-23 22:49:15 -0300

    Correção da propriedade ID

--- Alura.Loja.Testes.ConsoleApp/Cliente.cs
+++ Alura.Loja.Testes.ConsoleApp/Cliente.cs
@@ -2,7 +2,7 @@
 {
     internal class Cliente
     {
-        public int Id { get; set };
+        public int Id { get; set; }
         public string Nome { get; set; }
         public Endereco Endereco { get; set; }
     }
